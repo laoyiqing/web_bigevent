8b9a59e28a8e1c9d5ee88c28d86ed6543c6bb651
laoyiqing <3221820948@qq.com>
2022-10-26 22:15:39 +0800

    wan成了注册和登录

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,9 +2,7 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(option){
-    console.log(option.url);
     // 真正地发起Ajax请求的之前，统一拼接请求的根路径
     option.url = 'http://www.liulongbin.top:3007' + option.url
 
-    console.log(option.url);
 })
