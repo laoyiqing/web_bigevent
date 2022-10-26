829261ef1620839c71ed45f53707d2a5da5c1e7f
laoyiqing <3221820948@qq.com>
2022-10-26 22:02:34 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,10 @@
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(option){
+    console.log(option.url);
+    // 真正地发起Ajax请求的之前，统一拼接请求的根路径
+    option.url = 'http://www.liulongbin.top:3007' + option.url
+
+    console.log(option.url);
+})
