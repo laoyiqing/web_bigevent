ee4a6d7839e390581d3d07468917169c88c19f29
laoyiqing <3221820948@qq.com>
2022-10-29 10:40:27 +0800

    完成个人中心功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,7 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(option){
     // 真正地发起Ajax请求的之前，统一拼接请求的根路径
-    option.url = 'http://www.liulongbin.top:3007' + option.url
+    option.url = 'http://api-breakingnews-web.itheima.net' + option.url
 
     // 统一为有权限的接口，配置请求头
     if(option.url.indexOf('/my/') !== -1){
